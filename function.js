5e49cc96e0e54ed281154844e761544a6d1c2b41
Mariana Seiko <marianaseikofuku@gmail.com>
2024-11-06 16:12:15 -0300

    Atualizando

--- function.js
+++ function.js
@@ -1,28 +1,26 @@
 btCalcular.addEventListener('click', calcular);
+btLimpar.addEventListener('click', limpar);
 
-function abc(somaXY, somaX2, somaX2Y, n, mediaX, mediaY, somaY, somaX) {
+function abc(n, somaX, somaY, somaX2, somaX3, somaX4, somaXY, somaX2Y) {
     console.log("chamou abc");
-    var sxx = somaX2 - (n * Math.pow(mediaX, 2));
-    var sxy = somaXY - (n * mediaX * mediaY);
-    var sx2y = somaX2Y - (n * Math.pow(mediaX, 2) * mediaY);
-    var a = ((sx2y * sxx) - (sxy * somaX2)) / ((sxx * somaX2) - Math.pow(somaX2, 2));
-    var b = ((sxy * somaX2) - (sx2y * somaX2)) / ((sxx * somaX2) - Math.pow(somaX2, 2));
+    var sxx = somaX2 - (Math.pow(somaX, 2) / n);
+    var sxy = somaXY - ((somaX * somaY) / n);
+    var sxx2 = somaX3 - ((somaX2 * somaX) / n);
+    var sx2y = somaX2Y - ((somaX2 * somaY) / n);
+    var sx2x2 = somaX4 - (Math.pow(somaX2, 2) / n);
+    console.log("sxx: ", sxx, "sxy: ", sxy, "sxx2: ", sxx2, "sx2y: ", sx2y, "sx2x2: ", sx2x2);
+    var a = ((sx2y * sxx) - (sxy * sxx2)) / ((sxx * sx2x2) - (Math.pow(sxx2, 2)));
+    var b = ((sxy * sx2x2) - (sx2y * sxx2)) / ((sxx * sx2x2) - Math.pow(sxx2, 2));
     var c = (somaY / n) - (b * (somaX / n)) - (a * (somaX2 / n));
     console.log(a, b, c);
     return [a, b, c];
 }
 
-function equação(a, b, c, vetorY, vetorX, mediaY, n) {
-    console.log("chamou equação");
-    var sse = 0, sst = 0;
-    for (i = 0; i < n; i++) {
-        sse += Math.pow((vetorY[i] - (a * Math.pow(vetorX[i], 2)) - (b * vetorX[i] - c)), 2);
-        sst += Math.pow((vetorY[i] - mediaY), 2);
-    }
-    var r = 1 - (sse / sst);
-    var r2 = Math.pow(r, 2);
-    console.log(r, r2);
-    return [r, r2];
+function coeficiente (vExpl, vNExpl) {
+    console.log("chamou coeficiente");
+    var r2 = vExpl / (vExpl + vNExpl);
+    console.log(r2);
+    return r2;
 }
 
 function calcular() {
@@ -33,39 +31,82 @@ function calcular() {
     var n = vetorX.length;
     console.log(vetorX);
     console.log(vetorY);
-    if (document.getElementById("vX").value = "") {
-        msgErroX.innerHTML = "<p class='erro'>Campo obrigatório</p>";
+
+    if (document.getElementById("vX").value == "") {
+        document.getElementById("msgErroX").innerHTML = "<p class='erro'>Campo obrigatório</p>";
     }
-    else if (document.getElementById("vY").value = "") {
-        msgErroY.innerHTML = "<p class='erro'>Campo obrigatório</p>";
+    else if (document.getElementById("vY").value == "") {
+        document.getElementById("msgErroY").innerHTML = "<p class='erro'>Campo obrigatório</p>";
     }
     else if (vetorX.length != vetorY.length) {
-        msgErro.innerHTML = "<p class='erro'>A quantidade de valores X e Y tem que ser iguais</p>"
+        document.getElementById("msgErro").innerHTML = "<p class='erro'>A quantidade de valores X e Y tem que ser iguais</p>";
     }
     else {
-        var somaX = 0, somaY = 0, somaXY = 0, somaX2 = 0, somaX2Y = 0, somaX3 = 0, somaX4 = 0, vetorXY = [], vetorX2 = [], vetorX2Y = [], vetorX3 = [], vetorX4 = [];
-        for (var i = 0; i < n; i++) {
+
+        document.getElementById("msgErroX").innerHTML = "";
+        document.getElementById("msgErroY").innerHTML = "";
+        document.getElementById("msgErro").innerHTML = "";
+
+        var somaX = 0, somaY = 0, vetorX2 = [], somaX2 = 0, vetorX3 = [], somaX3 = 0, vetorX4 = [], somaX4 = 0, vetorXY = [], somaXY = 0, vetorX2Y = [], somaX2Y = 0;
+        
+        for (let i = 0; i < n; i++) {
             somaX += vetorX[i];
             somaY += vetorY[i];
-            somaXY += vetorX[i] * vetorY[i];
-            vetorXY.push(vetorX[i] * vetorY[i]);
             somaX2 += Math.pow(vetorX[i], 2);
-            vetorX2.push(Math.pow(vetorX[i], 2));
-            somaX2Y += Math.pow(vetorX[i], 2) * vetorY[i];
-            vetorX2Y.push(Math.pow(vetorX[i], 2) * vetorY[i]);
+            vetorX2[i] = Math.pow(vetorX[i], 2);
             somaX3 += Math.pow(vetorX[i], 3);
-            vetorX3.push(Math.pow(vetorX[i], 3));
+            vetorX3[i] = Math.pow(vetorX[i], 3);
             somaX4 += Math.pow(vetorX[i], 4);
-            vetorX4.push(Math.pow(vetorX[i], 4));
+            vetorX4[i] = Math.pow(vetorX[i], 4);
+            somaXY += (vetorX[i] * vetorY[i]);
+            vetorXY[i] = (vetorX[i] * vetorY[i]);
+            somaX2Y += (vetorX2[i] * vetorY[i]);
+            vetorX2Y[i] = (vetorX2[i] * vetorY[i]);
         }
-        var mediaX = somaX / n, mediaX2 = somaX2 / n, mediaY = somaY / n;
-        var [a, b, c] = abc(somaX, somaY, somaXY, somaX2, somaX2Y, somaX3, somaX4, n, mediaX, mediaX2, mediaY, somaY, somaX);
-        var [r, r2] = equação(resulabc.valorA, resulabc.valorB, resulabc.valorC, vetorY, vetorX, mediaY, n);
-        console.log("retornou equação");
-        somaN.innerHTML = "A quantidade de elementos é: " + n;
-        for (var i = 0; i < vetorX.length; i++) {
-            tabela.innerHTML += "<tr> <td>" + vetorX[i] + "</td> <td>" + vetorY[i] + "</td> <td>" + vetorXY[i] +"</td> <td>" + vetorX2[i] + "</td> <td>" + vetorX2Y[i] + "</td> <td>" + vetorX3[i] + "</td> <td>" + vetorX4[i] + "</td> </tr> <br>";
+
+        console.log("somaX: ", somaX, " somaY: ", somaY, " somaX2: ", somaX2, " somaX3: ", somaX3, " somaX4: ", somaX4, " somaXY: ", somaXY, " somaX2Y: ", somaX2Y);
+        console.log("vetorX2: ", vetorX2);
+        console.log("vetorX3: ", vetorX3);
+        console.log("vetorX4: ", vetorX4);
+        console.log("vetorXY: ", vetorXY);
+        console.log("vetorX2Y: ", vetorX2Y);
+
+        var [a, b, c] = abc(n, somaX, somaY, somaX2, somaX3, somaX4, somaXY, somaX2Y);
+        console.log(a, b, c);
+        vA = a, vB = b, vC = c;
+        var somaYest, vetorYest = [], vExpl = 0, vNExpl = 0, mediaY = somaY / n;
+
+        for (let i = 0; i < n; i++) {
+            somaYest += (vA * Math.pow(vetorX[i], 2)) + (vB * vetorX[i]) + vC;
+            vetorYest[i] = (vA * Math.pow(vetorX[i], 2)) + (vB * vetorX[i]) + vC;
+            vExpl += Math.pow((vetorYest[i] - mediaY), 2);
+            vNExpl += Math.pow((vetorYest[i] - vetorY[i]), 2);
         }
-        dados.innerHTML = "O valor de A é: " + a + ". O valor de B é: " + b + ".O valor de c é: " + c + ". <br>O coeficiente de correlação é: " + r + ". O coeficiente de determinação é: " + r2 + ".";
+
+        console.log(vExpl, vNExpl);
+
+        var r2 = coeficiente(vExpl, vNExpl);
+
+        document.getElementById("somaN").innerHTML = "<p>A quantidade de elementos é: " + n + ".</p>";
+
+        var intabela = "<table> <thead> <tr> <th>X</th> <th>Y</th> <th>XY</th> <th>X<span>2</span></th> <th>X<span>2</span>Y</th> <th>X<span>3</span></th> <th>X<span>4</span></th> </tr> </thead> <tbody>";
+
+        for (let i = 0; i < n; i++) {
+            intabela += "<tr> <td>" + vetorX[i] + "</td> <td>" + vetorY[i] + "</td> <td>" + vetorXY[i] + "</td> <td>" + vetorX2[i] + "</td> <td>" + vetorX2Y[i] + "</td> <td>" + vetorX3[i] + "</td> <td>" + vetorX4[i] + "</td> </tr>";
+        }
+
+        intabela += "</tbody> </table>"
+
+        document.getElementById("tabela").innerHTML = intabela;
+
+        document.getElementById("dados").innerHTML = "<p>O valor de A é: " + a + ". <br>O valor de B é: " + b + ". <br>O valor de c é: " + c + ". <br>O coeficiente de determinação é: " + r2 + ".</p>";
     }
+}
+
+function limpar() {
+    document.getElementById("vX").value = "";
+    document.getElementById("vY").value = "";
+    document.getElementById("somaN").innerHTML = "";
+    document.getElementById("tabela").innerHTML = "";
+    document.getElementById("dados").innerHTML = "";
 }
